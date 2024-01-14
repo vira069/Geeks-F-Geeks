47dd911ef3a195685546fdb01ed7f83a9327228e
Ravi Kumar <121314244+vira069@users.noreply.github.com>
2024-01-14 07:45:58 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Subarray with given sum/subarray-with-given-sum.java
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main{
+	static BufferedReader br;
+    static PrintWriter ot;
+    public static void main(String[] args) throws IOException{
+        
+        br = new BufferedReader(new InputStreamReader(System.in));
+        ot = new PrintWriter(System.out);
+
+        int t = Integer.parseInt(br.readLine());
+
+        while(t-->0){
+            
+            String s[] = br.readLine().trim().split(" ");
+            
+            int n = Integer.parseInt(s[0]);
+            int k = Integer.parseInt(s[1]);
+            int a[] = new int[n];
+            s = br.readLine().trim().split(" ");
+            for(int i = 0; i < n; i++)
+                a[i] = Integer.parseInt(s[i]);
+            Solution obj = new Solution();
+            ArrayList<Integer> res = obj.subarraySum(a, n, k);
+            for(int ii = 0;ii<res.size();ii++)
+                ot.print(res.get(ii) + " ");
+            ot.println();
+        }
+        ot.close();
+    }
+
+}
+// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to find a continuous sub-array which adds up to a given number.
+    static ArrayList<Integer> subarraySum(int[] arr, int n, int s) 
+    {
+        // Your code here
+        ArrayList<Integer>indexofSubArr=new ArrayList<>();
+        
+        int left = 0;
+        int tempSum = 0;
+      for(int right = 0;right<n;right++){
+          tempSum +=arr[right];
+          
+          while(tempSum>s && s!=0){
+               tempSum-=arr[left];
+               left++;
+           }
+           if(tempSum==s){
+              indexofSubArr.add(left+1);
+              indexofSubArr.add(right+1);
+              return indexofSubArr;
+           }
+        }
+        indexofSubArr.add(-1);
+        return indexofSubArr;
+    }
+}
