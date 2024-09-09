48911ffad55ed5ff28c2f1a48e7457f82ed756fa
Ravi Kumar <121314244+vira069@users.noreply.github.com>
2024-09-09 17:59:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Same characters in two strings/same-characters-in-two-strings.java
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*; 
+
+class GFG{
+    public static void main(String args[]) throws IOException { 
+        Scanner sc = new Scanner(System.in);
+        int t = Integer.parseInt(sc.nextLine().trim());
+        while(t-- > 0){
+            String A = sc.nextLine().trim();
+            String B = sc.nextLine().trim();
+            Solution obj = new Solution();
+            System.out.println(obj.sameChar(A, B));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution 
+{ 
+    int sameChar(String A, String B) 
+    {
+        // code here
+         A = A.toLowerCase();
+         B = B.toLowerCase();
+        int count=0;
+        for(int i=0;i<A.length();i++){
+            if(A.charAt(i)==B.charAt(i)){
+                count++;
+            }
+        }
+        return count;
+    }
+} 
