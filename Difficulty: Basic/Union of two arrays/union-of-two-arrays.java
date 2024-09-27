0ed2af7eba7126fe86912a5c9890dda9bf7d2be4
Ravi Kumar <121314244+vira069@users.noreply.github.com>
2024-09-27 23:34:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Union of two arrays/union-of-two-arrays.java
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine().trim());
+
+        while (t-- > 0) {
+            // Reading the first array
+            String line1 = read.readLine().trim();
+            String[] numsStr1 = line1.split(" ");
+            int[] a = new int[numsStr1.length];
+            for (int i = 0; i < numsStr1.length; i++) {
+                a[i] = Integer.parseInt(numsStr1[i]);
+            }
+
+            // Reading the second array
+            String line2 = read.readLine().trim();
+            String[] numsStr2 = line2.split(" ");
+            int[] b = new int[numsStr2.length];
+            for (int i = 0; i < numsStr2.length; i++) {
+                b[i] = Integer.parseInt(numsStr2[i]);
+            }
+
+            // Creating an instance of the Solution class
+            Solution ob = new Solution();
+
+            // Calling doUnion method and printing the result
+            System.out.println(ob.doUnion(a, b));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public static int doUnion(int arr1[], int arr2[]) {
+        // Your code here\
+         HashSet<Integer>set = new HashSet<>();
+        for(int ae : arr1) set.add(ae);
+        for(int ae : arr2) set.add(ae);
+        return set.size();
+    }
+}
