8b7c8ac68d6be6db333bdaa3087a222f621a755d
Ravi Kumar <121314244+vira069@users.noreply.github.com>
2025-02-20 23:42:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Pattern 2/pattern-2.java
@@ -0,0 +1,30 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Main {
+    // Driver code
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            Solution obj = new Solution();
+            obj.printTriangle(n);
+        
+System.out.println("~");
+}
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+
+    void printTriangle(int n) {
+        for (int i = 1; i <= n; i++) {
+        System.out.println("* ".repeat(i).trim());
+    }
+    }
+}
